f51156d188c1dcc9ac1cba1270084024b5d7dbef
Roman Scherer <roman@burningswell.com>
2010-09-03 12:40:12 +0200

    Import the new Hadoop API.

--- src/clojure_hadoop/imports.clj
+++ src/clojure_hadoop/imports.clj
@@ -1,106 +1,126 @@
 (ns clojure-hadoop.imports
-;;^{:doc "Functions to import entire packages under org.apache.hadoop."}
-)
+  "Functions to import entire packages under org.apache.hadoop.")
 
 (defn import-conf
   "Imports all classes/interfaces/exceptions from the package
   org.apache.hadoop.conf into the current namespace."
   []
-  (import '(org.apache.hadoop.conf Configurable Configuration
-Configuration$IntegerRanges Configured)))
+  (import
+   '(org.apache.hadoop.conf
+     Configurable Configuration Configuration$IntegerRanges Configured)))
 
 (defn import-io
   "Imports all classes/interfaces/exceptions from the package
   org.apache.hadoop.io into the current namespace."
   []
-  (import '(org.apache.hadoop.io RawComparator
-SequenceFile$Sorter$RawKeyValueIterator SequenceFile$ValueBytes
-Stringifier Writable WritableComparable WritableFactory
-AbstractMapWritable ArrayFile ArrayFile$Reader ArrayFile$Writer
-ArrayWritable BooleanWritable BooleanWritable$Comparator BytesWritable
-BytesWritable$Comparator ByteWritable ByteWritable$Comparator
-CompressedWritable DataInputBuffer DataOutputBuffer DefaultStringifier
-DoubleWritable DoubleWritable$Comparator FloatWritable
-FloatWritable$Comparator GenericWritable InputBuffer LongWritable
-LongWritable$Comparator IOUtils IOUtils$NullOutputStream LongWritable
-LongWritable$Comparator LongWritable$DecreasingComparator MapFile
-MapFile$Reader MapFile$Writer MapWritable MD5Hash MD5Hash$Comparator
-NullWritable NullWritable$Comparator ObjectWritable OutputBuffer
-SequenceFile SequenceFile$Metadata SequenceFile$Reader
-SequenceFile$Sorter SequenceFile$Writer SetFile SetFile$Reader
-SetFile$Writer SortedMapWritable Text Text$Comparator
-TwoDArrayWritable UTF8 UTF8$Comparator VersionedWritable VLongWritable
-VLongWritable WritableComparator WritableFactories WritableName
-WritableUtils SequenceFile$CompressionType MultipleIOException
-VersionMismatchException)))
+  (import
+   '(org.apache.hadoop.io
+     RawComparator SequenceFile$Sorter$RawKeyValueIterator SequenceFile$ValueBytes
+     Stringifier Writable WritableComparable WritableFactory
+     AbstractMapWritable ArrayFile ArrayFile$Reader ArrayFile$Writer
+     ArrayWritable BooleanWritable BooleanWritable$Comparator BytesWritable
+     BytesWritable$Comparator ByteWritable ByteWritable$Comparator
+     CompressedWritable DataInputBuffer DataOutputBuffer DefaultStringifier
+     DoubleWritable DoubleWritable$Comparator FloatWritable
+     FloatWritable$Comparator GenericWritable InputBuffer IntWritable LongWritable
+     LongWritable$Comparator IOUtils IOUtils$NullOutputStream LongWritable
+     LongWritable$Comparator LongWritable$DecreasingComparator MapFile
+     MapFile$Reader MapFile$Writer MapWritable MD5Hash MD5Hash$Comparator
+     NullWritable NullWritable$Comparator ObjectWritable OutputBuffer
+     SequenceFile SequenceFile$Metadata SequenceFile$Reader
+     SequenceFile$Sorter SequenceFile$Writer SetFile SetFile$Reader
+     SetFile$Writer SortedMapWritable Text Text$Comparator
+     TwoDArrayWritable UTF8 UTF8$Comparator VersionedWritable VLongWritable
+     VLongWritable WritableComparator WritableFactories WritableName
+     WritableUtils SequenceFile$CompressionType MultipleIOException
+     VersionMismatchException)))
 
 (defn import-io-compress
   "Imports all classes/interfaces/exceptions from the package
   org.apache.hadoop.io.compress into the current namespace."
   []
-  (import '(org.apache.hadoop.io.compress CompressionCodec Compressor
-Decompressor BlockCompressorStream BlockDecompressorStream CodecPool
-CompressionCodecFactory CompressionInputStream CompressionOutputStream
-CompressorStream DecompressorStream DefaultCodec GzipCodec
-GzipCodec$GzipInputStream GzipCodec$GzipOutputStream)))
+  (import
+   '(org.apache.hadoop.io.compress
+     CompressionCodec Compressor Decompressor BlockCompressorStream
+     BlockDecompressorStream BZip2Codec CodecPool CompressionCodecFactory
+     CompressionInputStream CompressionOutputStream CompressorStream
+     DecompressorStream DefaultCodec GzipCodec GzipCodec$GzipInputStream
+     GzipCodec$GzipOutputStream)))
 
 (defn import-fs
   "Imports all classes/interfaces/exceptions from the package
   org.apache.hadoop.fs into the current namespace."
   []
-  (import '(org.apache.hadoop.fs PathFilter PositionedReadable
-Seekable Syncable BlockLocation BufferedFSInputStream
-ChecksumFileSystem ContentSummary DF DU FileStatus FileSystem
-FileSystem$Statistics FileUtil FileUtil$HardLink FilterFileSystem
-FSDataInputStream FSDataOutputStream FSInputChecker FSInputStream
-FSOutputSummer FsShell FsUrlStreamHandlerFactory HarFileSystem
-InMemoryFileSystem LocalDirAllocator LocalFileSystem Path
-RawLocalFileSystem Trash ChecksumException FSError)))
+  (import
+   '(org.apache.hadoop.fs
+     PathFilter PositionedReadable Seekable Syncable BlockLocation
+     BufferedFSInputStream ChecksumFileSystem ContentSummary DF DU
+     FileStatus FileSystem FileSystem$Statistics FileUtil FileUtil$HardLink
+     FilterFileSystem FSDataInputStream FSDataOutputStream FSInputChecker
+     FSInputStream FSOutputSummer FsShell FsUrlStreamHandlerFactory HarFileSystem
+     InMemoryFileSystem LocalDirAllocator LocalFileSystem Path RawLocalFileSystem
+     Trash ChecksumException FSError)))
 
-(defn import-mapred
+(defn import-mapreduce
   "Imports all classes/interfaces/exceptions from the package
-  org.apache.hadoop.mapred into the current namespace."
+  org.apache.hadoop.mapreduce into the current namespace."
   []
-  (import '(org.apache.hadoop.mapred InputFormat InputSplit
-JobConfigurable JobHistory$Listener Mapper MapRunnable OutputCollector
-OutputFormat Partitioner RawKeyValueIterator RecordReader RecordWriter
-Reducer Reporter RunningJob SequenceFileInputFilter$Filter
-ClusterStatus Counters Counters$Counter Counters$Group
-DefaultJobHistoryParser FileInputFormat FileOutputFormat FileSplit ID
-IsolationRunner JobClient JobConf JobEndNotifier JobHistory
-JobHistory$HistoryCleaner JobHistory$JobInfo JobHistory$MapAttempt
-JobHistory$ReduceAttempt JobHistory$Task JobHistory$TaskAttempt JobID
-JobProfile JobStatus JobTracker KeyValueLineRecordReader
-KeyValueTextInputFormat LineRecordReader LineRecordReader$LineReader
-MapFileOutputFormat MapReduceBase MapRunner MultiFileInputFormat
-MultiFileSplit OutputLogFilter SequenceFileAsBinaryInputFormat
-SequenceFileAsBinaryInputFormat$SequenceFileAsBinaryRecordReader
-SequenceFileAsBinaryOutputFormat
-SequenceFileAsBinaryOutputFormat$WritableValueBytes
-SequenceFileAsTextInputFormat SequenceFileAsTextRecordReader
-SequenceFileInputFilter SequenceFileInputFilter$FilterBase
-SequenceFileInputFilter$MD5Filter
-SequenceFileInputFilter$PercentFilter
-SequenceFileInputFilter$RegexFilter SequenceFileInputFormat
-SequenceFileOutputFormat SequenceFileRecordReader TaskAttemptID
-TaskCompletionEvent TaskID TaskLog TaskLogAppender TaskLogServlet
-TaskReport TaskTracker TaskTracker$MapOutputServlet TextInputFormat
-TextOutputFormat TextOutputFormat$LineRecordWriter
-JobClient$TaskStatusFilter JobHistory$Keys JobHistory$RecordTypes
-JobHistory$Values JobPriority JobTracker$State
-TaskCompletionEvent$Status TaskLog$LogName FileAlreadyExistsException
-InvalidFileTypeException InvalidInputException InvalidJobConfException
-JobTracker$IllegalStateException)))
+  (import
+   '(org.apache.hadoop.mapreduce
+     Counter CounterGroup Counters ID InputFormat InputSplit Job
+     Job$JobState JobContext JobID MapContext Mapper OutputCommitter
+     OutputFormat Partitioner RecordReader RecordWriter ReduceContext
+     Reducer StatusReporter TaskAttemptContext TaskAttemptID TaskID
+     TaskInputOutputContext)))
 
-(defn import-mapred-lib
+(defn import-mapreduce-lib-input
   "Imports all classes/interfaces/exceptions from the package
-  org.apache.hadoop.mapred.lib into the current namespace."
+  org.apache.hadoop.mapreduce.lib.input into the current namespace."
   []
-  (import '(org.apache.hadoop.mapred.lib FieldSelectionMapReduce
-HashPartitioner IdentityMapper IdentityReducer InverseMapper
-KeyFieldBasedPartitioner LongSumReducer MultipleOutputFormat
-MultipleSequenceFileOutputFormat MultipleTextOutputFormat
-MultithreadedMapRunner NLineInputFormat NullOutputFormat RegexMapper
-TokenCountMapper)))
+  (import
+   '(org.apache.hadoop.mapreduce.lib.input
+     FileInputFormat FileSplit LineRecordReader SequenceFileInputFormat
+     SequenceFileRecordReader TextInputFormat InvalidInputException)))
+
+(defn import-mapreduce-lib-map
+  "Imports all classes/interfaces/exceptions from the package
+  org.apache.hadoop.mapreduce.lib.map into the current namespace."
+  []
+  (import
+   '(org.apache.hadoop.mapreduce.lib.map
+     InverseMapper MultithreadedMapper TokenCounterMapper)))
+
+(defn import-mapreduce-lib-output
+  "Imports all classes/interfaces/exceptions from the package
+  org.apache.hadoop.mapreduce.lib.output into the current namespace."
+  []
+  (import
+   '(org.apache.hadoop.mapreduce.lib.output
+     FileOutputCommitter FileOutputFormat NullOutputFormat
+     SequenceFileOutputFormat TextOutputFormat TextOutputFormat$LineRecordWriter)))
+
+(defn import-mapreduce-lib-partition
+  "Imports all classes/interfaces/exceptions from the package
+  org.apache.hadoop.mapreduce.lib.partition into the current namespace."
+  []
+  (import
+   '(org.apache.hadoop.mapreduce.lib.partition HashPartitioner)))
+
+(defn import-mapreduce-lib-reduce
+  "Imports all classes/interfaces/exceptions from the package
+  org.apache.hadoop.mapreduce.lib.reduce into the current namespace."
+  []
+  (import
+   '(org.apache.hadoop.mapreduce.lib.reduce IntSumReducer LongSumReducer)))
+
+(defn import-mapreduce-lib
+  "Imports all classes/interfaces/exceptions from the package
+  org.apache.hadoop.mapreduce.lib into the current namespace."
+  []
+  (import-mapreduce-lib-input)
+  (import-mapreduce-lib-map)
+  (import-mapreduce-lib-output)
+  (import-mapreduce-lib-partition)
+  (import-mapreduce-lib-reduce))
 
 
--- test/clojure_hadoop/test/imports.clj
+++ test/clojure_hadoop/test/imports.clj
@@ -1,11 +1,32 @@
 (ns clojure-hadoop.test.imports
-  (:require [clojure-hadoop.imports :as imp])
-  (:use clojure.test))
+  (:use clojure.test clojure-hadoop.imports))
 
-(deftest test-imports
-  (imp/import-conf)
-  (imp/import-io)
-  (imp/import-io-compress)
-  (imp/import-fs)
-  (imp/import-mapred)
-  (imp/import-mapred-lib))
+(deftest test-import-conf
+  (import-conf))
+
+(deftest test-import-io
+  (import-io))
+
+(deftest test-import-io-compress
+  (import-io-compress))
+
+(deftest test-import-fs
+  (import-fs))
+
+(deftest test-import-mapreduce
+  (import-mapreduce))
+
+(deftest test-import-mapreduce-lib-input
+  (import-mapreduce-lib-input))
+
+(deftest test-import-mapreduce-lib-map
+  (import-mapreduce-lib-map))
+
+(deftest test-import-mapreduce-lib-output
+  (import-mapreduce-lib-output))
+
+(deftest test-import-mapreduce-lib-partition
+  (import-mapreduce-lib-partition))
+
+(deftest test-import-mapreduce-lib
+  (import-mapreduce-lib))
