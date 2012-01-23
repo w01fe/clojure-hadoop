93d0f54ada0da2ecd62b9f1854a6da3b9c9211ce
Christopher Miles <twitch@nervestaple.com>
2012-01-23 08:46:09 -0500

    Merged "project.clj" from eslick. Increased the version number to 1.4. Changed Hadoop dependency from 0.20.205.0 to 0.20.2. This let me remove the jackson dependency. Added development dependency on swank-clojure.

--- project.clj
+++ project.clj
@@ -1,17 +1,14 @@
-(defproject clojure-hadoop "1.3.3-SNAPSHOT"
+(defproject clojure-hadoop "1.4"
   :description "Library to aid writing Hadoop jobs in Clojure."
   :url "http://github.com/cmiles74/clojure-hadoop"
   :license {:name "Eclipse Public License 1.0"
             :url "http://opensource.org/licenses/eclipse-1.0.php"
             :distribution "repo"
             :comments "Same license as Clojure"}
-  :dependencies [[org.apache.hadoop/hadoop-core "0.20.205.0"]
-                 [org.clojure/clojure "1.3.0"]
-                 [org.codehaus.jackson/jackson-mapper-asl "1.9.2"]
-                 [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
-                                                    javax.jms/jms
-                                                    com.sun.jdmk/jmxtools
-                                                    com.sun.jmx/jmxri]]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.apache.hadoop/hadoop-core "0.20.2"]
+                 [log4j/log4j "1.2.16"]]
+  :dev-dependencies [[swank-clojure "1.3.3"]]
   :aot [clojure-hadoop.config
         clojure-hadoop.defjob
         clojure-hadoop.gen
@@ -19,6 +16,7 @@
         clojure-hadoop.job
         clojure-hadoop.load
         clojure-hadoop.wrap
+	clojure-hadoop.flow
         ;; TODO: Remove them? Only needed for the tests.
         clojure-hadoop.examples.wordcount1
         clojure-hadoop.examples.wordcount2])
