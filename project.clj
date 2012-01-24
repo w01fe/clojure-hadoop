b242d6aef84a883132886b465ea5ddfa2da1aa9a
Alex Ott <alexott@gmail.com>
2012-01-24 18:44:25 +0100

    Make working after merge
    Hadoop 0.20.205.0 exists only in Apache's maven repository

--- project.clj
+++ project.clj
@@ -5,9 +5,18 @@
             :url "http://opensource.org/licenses/eclipse-1.0.php"
             :distribution "repo"
             :comments "Same license as Clojure"}
+  :repositories {"apache" { :url "https://repository.apache.org/content/repositories/releases/"
+                           :snapshots false
+                           :releases {:checksum :fail :update :always}}
+                 }
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache.hadoop/hadoop-core "0.20.2"]
-                 [log4j/log4j "1.2.16"]]
+                 [org.apache.hadoop/hadoop-core "0.20.205.0"]
+                 [org.codehaus.jackson/jackson-mapper-asl "1.9.2"]
+                 [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jdmk/jmxtools
+                                                    com.sun.jmx/jmxri]]
+                 ]
   :dev-dependencies [[swank-clojure "1.3.4"]]
   :aot [clojure-hadoop.config
         clojure-hadoop.defjob
