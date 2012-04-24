b49d41056852508401c34346ec1294266523fe55
Alex Ott <alexott@gmail.com>
2012-04-24 13:14:33 +0200

    use Hadoop 1.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-hadoop "1.4.0"
+(defproject clojure-hadoop "1.4.1"
   :description "Library to aid writing Hadoop jobs in Clojure."
   :url "http://github.com/alexott/clojure-hadoop"
   :license {:name "Eclipse Public License 1.0"
@@ -6,13 +6,13 @@
             :distribution "repo"
             :comments "Same license as Clojure"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache.hadoop/hadoop-core "1.0.1"]
+                 [org.apache.hadoop/hadoop-core "1.0.2"]
                  [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
                                                     javax.jms/jms
                                                     com.sun.jdmk/jmxtools
                                                     com.sun.jmx/jmxri]]
                  ]
-  :dev-dependencies [[swank-clojure "1.4.0"]]
+  :dev-dependencies [[swank-clojure "1.4.2"]]
   :aot [clojure-hadoop.config
         clojure-hadoop.defjob
         clojure-hadoop.gen
