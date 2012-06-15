37abea48e6c8bb1f712cdd4012e8b9923873b34a
Michael Nygard <mtnygard@gmail.com>
2012-06-15 10:28:11 -0500

    Revved Hadoop version to 1.0.3

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             :distribution "repo"
             :comments "Same license as Clojure"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache.hadoop/hadoop-core "1.0.2"]
+                 [org.apache.hadoop/hadoop-core "1.0.3"]
                  [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
                                                     javax.jms/jms
                                                     com.sun.jdmk/jmxtools
