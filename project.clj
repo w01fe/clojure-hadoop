90db4bd6dc27e73a47158d071004029eccce371d
Alex Bahouth <roxxibear@gmail.com>
2013-03-17 11:25:55 -0700

    Hadoop 1.0.3 ~> 1.0.4; Added test dir to compilation path.
    1.0.4 is the latest version of hadoop. Bumped it up.
    
    If someone were to download and run lein test, it would fail because the test cases are not compiled. Added the directive in the project.clj to compile them so lein test passes.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             :distribution "repo"
             :comments "Same license as Clojure"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.apache.hadoop/hadoop-core "1.0.3"]
+                 [org.apache.hadoop/hadoop-core "1.0.4"]
                  ;; commons-io was omitted in 1.0.3/1.0.4
                  ;; needs to be explicitly included
                  ;; until this is resolved.                 
@@ -16,6 +16,7 @@
                                                     javax.jms/jms
                                                     com.sun.jdmk/jmxtools
                                                     com.sun.jmx/jmxri]]]
+  :source-paths ["src" "test"]
   :dev-dependencies [[swank-clojure "1.4.2"]]
   :aot [clojure-hadoop.config
         clojure-hadoop.defjob
