12735b952c89928a2257327328e226b8a7a1f6c9
Jason Wolfe <jason@w01fe.com>
2015-01-28 15:35:30 -0800

    Don't include hadoop as dependency

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject w01fe/clojure-hadoop "1.4.6-SNAPSHOT"
+(defproject w01fe/clojure-hadoop "1.4.7-SNAPSHOT"
   :description "Library to aid writing Hadoop jobs in Clojure."
   :url "http://github.com/w01fe/clojure-hadoop"
   :license {:name "Eclipse Public License 1.0"
             :url "http://opensource.org/licenses/eclipse-1.0.php"
             :distribution "repo"
             :comments "Same license as Clojure"}
-  :profiles {:provided {:dependencies [[org.clojure/clojure "1.6.0"]]}}
-  :dependencies [[org.apache.hadoop/hadoop-common "2.6.0"]
-                 [org.apache.hadoop/hadoop-mapreduce-client-core "2.6.0"]
-                 [org.apache.hadoop/hadoop-aws "2.6.0"]
-                 [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.6.0"]
+                                       [org.apache.hadoop/hadoop-common "2.6.0"]
+                                       [org.apache.hadoop/hadoop-mapreduce-client-core "2.6.0"]
+                                       [org.apache.hadoop/hadoop-aws "2.6.0"]]}}
+  :dependencies [[log4j/log4j "1.2.16" :exclusions [javax.mail/mail
                                                     javax.jms/jms
                                                     com.sun.jdmk/jmxtools
                                                     com.sun.jmx/jmxri]]]
